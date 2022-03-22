c44659de39021e337ff968e9bb42bfc6e92346dc
hugosenari <hugosenari@gmail.com>
2022-03-22 00:34:06 -0300

    chore: re-index ../nimpkgs/l/listenbrainz

--- 0_1_2/flake.nix
+++ 0_1_2/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-listenbrainz-0_1_2.flake = false;
   inputs.src-listenbrainz-0_1_2.owner = "tandy1000";
-  inputs.src-listenbrainz-0_1_2.ref   = "refs/tags/0.1.2";
+  inputs.src-listenbrainz-0_1_2.ref   = "0_1_2";
   inputs.src-listenbrainz-0_1_2.repo  = "listenbrainz-nim";
   inputs.src-listenbrainz-0_1_2.type  = "gitlab";
   
